433eacf7de122d559c6d91f20a3af3c9216dd9c1
GayanJayarathne <krgayab97@gmail.com>
2025-04-07 09:32:01 +0530

    jest updated

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx)$",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
